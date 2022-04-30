59b27be40faa3d83d37bc7d740204a4541917d82
nflhz <104671569+nflhz@users.noreply.github.com>
2022-04-30 18:28:32 +0700

    Add files via upload

---
+++ UTS-Looping.py
@@ -0,0 +1,19 @@
+#Pengulangan secara Manual
+print("YAKIN BISA")
+print("YAKIN BISA")
+
+#Pengulangan dengan jumlah tertentu
+    #jumlah yang ditentukan sebanyak 3x
+for x in range(3):
+    print("PASTI BISA")
+
+#Pengulangan dengan jumlah yang ditentukan
+    #pengulangan dilakukan selama n belum melebihi angka 20 dan dengan increment 5
+n = 0
+while(n<20):
+    n = n+5
+    print(n)
+
+
+    
+    
